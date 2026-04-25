6f34be9eea5190e3e583892a2544d493b1ee8590
subzeroid <143403577+subzeroid@users.noreply.github.com>
2026-04-25 17:00:54 +0300

    port: add explore mixin from instagrapi 2.4.4

---
+++ aiograpi/mixins/explore.py
@@ -0,0 +1,47 @@
+class ExploreMixin:
+    """
+    Helpers for the explore page
+    """
+
+    async def explore_page(self):
+        """
+        Get explore page
+
+        Returns
+        -------
+        dict
+        """
+        return await self.private_request("discover/topical_explore/")
+
+    async def report_explore_media(self, media_pk: int):
+        """
+        Report media in explore page. This is equivalent to the "not interested" button
+
+        Parameters
+        ----------
+        media_pk: int
+            Media PK
+        Returns
+        -------
+        bool
+            True if success
+        """
+        params = {
+            "m_pk": media_pk,
+        }
+        result = await self.private_request("discover/explore_report/", params=params)
+        return result["explore_report_status"] == "OK"
+
+    async def explore_page_media_info(self, media_pk: int):
+        """
+        Returns media information for a media item on the explore page
+
+        This is the API call that happens when you're on the explore page
+        and you click into a media item. It returns information about that media item
+        like comments, likes, etc.
+        """
+        return (
+            await self.private_request(
+                "/v1/discover/media_metadata/", params={"media_id": media_pk}
+            )
+        )["media_or_ad"]
